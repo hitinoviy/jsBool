22a741a4a3053b9b57db050150cb85ff64b91af7
Hitinoviy <vovan.korchagin.1999@gmail.com>
2020-11-06 11:01:05 +0300

    inicial commit

---
+++ test1.php
@@ -0,0 +1,14 @@
+<?php
+    $username = "Vladimir";
+    echo $username;
+    echo "<br>";
+    $copyUsername = $username;
+    echo $copyUsername;
+?>
+<?php
+    $x = array(array('x',' ','x'),
+                array(' ','x','o'),
+                array('x','o',' '));
+    echo $x[2][0]; // первое число по горизонтали, второе по вертикали 
+    // первое число номер массива, второе его элемент 
+?>
